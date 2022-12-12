3b987959174eb67bb47134b3494b58bdecb4c6c4
kevwilhelm95 <kwilhelm95@gmail.com>
2022-12-12 17:18:54 -0600

    Fix intermediate file path for exact test

--- src/OddsRatios.py
+++ src/OddsRatios.py
@@ -32,7 +32,7 @@ class GetOddsRatios():
     def RunExactTest(self):
         print("---Preparing intermediate files for ExactTest---")
         # Create intermediate files needed for ExactTest script
-        main_outpath = Path(self.oPath)[0]
+        main_outpath = Path(self.oPath).partents[0]
         intermediate_outpath = CreateDir(main_outpath, "IntermediateFiles/")
         CreateSampleOnlyFile(self.CaseControl, intermediate_outpath)
         CreateSampleFamFile(self.CaseControl, intermediate_outpath)
