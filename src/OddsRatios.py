c0b02137bd4b166b9f7d9146f28fafe2e47755cc
kevwilhelm95 <kwilhelm95@gmail.com>
2022-12-13 14:55:01 -0600

    Change to shell = False

--- src/OddsRatios.py
+++ src/OddsRatios.py
@@ -54,7 +54,7 @@ class GetOddsRatios():
                 intermediate_outpath]
         #proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = False)
         print("---ExactTest Script Started---")
-        subprocess.Popen(cmd, shell = True, check = True).wait()
+        subprocess.Popen(cmd, shell = False, check = True).wait()
 
         #stdout, stderr = proc.communicate()
         #print(stdout)
